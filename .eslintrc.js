00ea934fce7c5bd03b6a47a62c6e4c33434cefb8
luzhong <luzhong256@gmail.com>
2021-05-30 21:28:00 +0800

    添加echarts和高德地图

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+  ],
+  globals: {
+    'AMap': false,
+    'AMapUI': false
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+
+import VueAMap from 'vue-amap'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import {
+  CanvasRenderer
+} from 'echarts/renderers'
+import {
+  BarChart
+} from 'echarts/charts'
+import {
+  GridComponent,
+  TooltipComponent
+} from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  TooltipComponent
+]);
+
+// register globally (or you can do it locally)
+Vue.component('v-chart', ECharts)
+Vue.use(ElementUI)
+Vue.use(VueAMap);
+Vue.use(VueAxios, axios)
+
+VueAMap.initAMapApiLoader({
+  key: 'ce73658784de5e67a760ddca0c95bf8f',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor', 'AMap.Heatmap'],
+  v: '1.4.4'
+});
+
 Vue.config.productionTip = false
 
 new Vue({
